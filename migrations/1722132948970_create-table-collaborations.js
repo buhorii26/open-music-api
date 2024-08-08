17f85aaacd9fc41a0693867cb7c5aa2bd71530e2
buhorii26 <buhorics@gmail.com>
2024-08-08 22:18:24 +0900

    menghapus kode yg tidak terpakai pada migrations dan membuat table user album likes

--- migrations/1722132948970_create-table-collaborations.js
+++ migrations/1722132948970_create-table-collaborations.js
@@ -1,8 +1,3 @@
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
 exports.up = (pgm) => {
   // membuat table collaborations
   pgm.createTable('collaborations', {
---
+++ migrations/1723122396777_create-table-user-album-likes.js
@@ -0,0 +1,55 @@
+exports.up = (pgm) => {
+  pgm.createTable('user_album_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+  /*
+    memberikan constraint foreign key pada user_album_likes.user_id
+    terhadap users.id
+    */
+  pgm.addConstraint(
+    'user_album_likes',
+    'fk_user_album_likes.user_id_users.id',
+    {
+      foreignKeys: {
+        columns: 'user_id',
+        references: 'users(id)',
+        onDelete: 'CASCADE',
+      },
+    },
+  );
+  /*
+    memberikan constraint foreign key pada user_album_likes.album_id
+    terhadap albums.id
+    */
+  pgm.addConstraint(
+    'user_album_likes',
+    'fk_user_album_likes.album_id_albums.id',
+    {
+      foreignKeys: {
+        columns: 'album_id',
+        references: 'albums(id)',
+        onDelete: 'CASCADE',
+      },
+    },
+  );
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('user_album_likes');
+};
