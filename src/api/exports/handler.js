a460c772757fd2a9259db4bd96dc2531bf7fd155
buhorii26 <buhorics@gmail.com>
2024-08-10 13:04:15 +0900

    update kode export

--- src/api/exports/handler.js
+++ src/api/exports/handler.js
@@ -1,16 +1,35 @@
 const autoBind = require('auto-bind');
 
 class ExportsHandler {
-  constructor(service, validator) {
+  constructor(service, playlistsService, collaborationsService, validator) {
     this._service = service;
+    this._playlistsService = playlistsService;
+    this._collaborationsService = collaborationsService;
     this._validator = validator;
     autoBind(this);
   }
 
   async postExportPlaylistsHandler(request, h) {
-    this._validator.validateExportNotesPayload(request.payload);
+    this._validator.validateExportPlaylistsPayload(request.payload);
+    const { id: playlistId } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+    // Verifikasi akses ke playlist (pemilik atau kolaborator)
+    try {
+      await this._playlistsService.verifyPlaylistSongsAccess(playlistId, credentialId);
+    } catch (error) {
+      console.log('User is not the owner, checking collaborator:', error.message);
+      try {
+        await this._collaborationsService.verifyCollaborator(playlistId, credentialId);
+      } catch (collaborationError) {
+        console.log('User is not a collaborator, retrying playlist access:', collaborationError.message);
+        // Mengembalikan verifikasi playlist access setelah kolaborasi dihapus
+        await this._playlistsService.verifyPlaylistSongsAccess(playlistId, credentialId);
+      }
+    }
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+    const playlists = await this._playlistsService.getPlaylists(playlistId);
     const message = {
-      userId: request.auth.credentials.id,
+      playlists,
       targetEmail: request.payload.targetEmail,
     };
     await this._service.sendMessage(
@@ -19,7 +38,7 @@ class ExportsHandler {
     );
     const response = h.response({
       status: 'success',
-      message: 'Permintaan Anda dalam antrean',
+      message: 'Permintaan Anda sedang kami proses!',
     });
     response.code(201);
     return response;
--- src/api/exports/index.js
+++ src/api/exports/index.js
@@ -1,11 +1,21 @@
 const ExportsHandler = require('./handler');
 const routes = require('./routes');
+const PlaylistsService = require('../../services/postgre/PlaylistsService');
+const CollaborionsService = require('../../services/postgre/CollaborationsService');
+
+const playlistsService = new PlaylistsService();
+const collaborationsService = new CollaborionsService();
 
 module.exports = {
   name: 'exports',
   version: '1.0.0',
   register: async (server, { service, validator }) => {
-    const exportsHandler = new ExportsHandler(service, validator);
+    const exportsHandler = new ExportsHandler(
+      service,
+      playlistsService,
+      collaborationsService,
+      validator,
+    );
     server.route(routes(exportsHandler));
   },
 };
